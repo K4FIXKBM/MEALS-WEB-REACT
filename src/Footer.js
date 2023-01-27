212214e6a2ca67686998a54a52a0a41ff64a5832
K4FIXKBM <kafisarder@gmail.com>
2023-01-27 20:13:35 +0600

    React Meal Web

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer(){
+    return <div style={{backgroundColor:'blue'}}>
+        <p style={{color:'white'}}>Designed and Developed By</p>
+        <p style={{color:'white'}}>K.Sathyaprakash Reddy</p>
+    </div>
+}
+export default Footer
